6cad0f0f9088b3ac279171f67f392fba8e4102d3
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2023-06-15 11:10:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import About from './pages/About';
