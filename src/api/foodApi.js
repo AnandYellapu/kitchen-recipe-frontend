214dbcd5798abf7653c5c4f42459e5a7235aeecd
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2023-06-15 11:18:08 +0530

    Update foodApi.js

--- src/api/foodApi.js
+++ src/api/foodApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Get all foods
 export const getFoods = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/api/food');
+    const response = await axios.get('https://kitchen-recipe.onrender.com//api/food');
     return response.data;
   } catch (error) {
     console.error(error);
@@ -14,7 +14,7 @@ export const getFoods = async () => {
 // Get food by ID
 export const getFoodById = async (id) => {
   try {
-    const response = await axios.get(`http://localhost:8000/api/food/${id}`);
+    const response = await axios.get(`https://kitchen-recipe.onrender.com//api/food/${id}`);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -25,7 +25,7 @@ export const getFoodById = async (id) => {
 // Create a new food
 export const createFood = async (foodData) => {
   try {
-    const response = await axios.post('http://localhost:8000/api/food', foodData);
+    const response = await axios.post('https://kitchen-recipe.onrender.com//api/food', foodData);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -36,7 +36,7 @@ export const createFood = async (foodData) => {
 // Update an existing food
 export const updateFood = async (id, foodData) => {
   try {
-    const response = await axios.put(`http://localhost:8000/api/food/${id}`, foodData);
+    const response = await axios.put(`https://kitchen-recipe.onrender.com//api/food/${id}`, foodData);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -47,7 +47,7 @@ export const updateFood = async (id, foodData) => {
 // Delete a food
 export const deleteFood = async (id) => {
   try {
-    const response = await axios.delete(`http://localhost:8000/api/food/${id}`);
+    const response = await axios.delete(`https://kitchen-recipe.onrender.com//api/food/${id}`);
     return response.data;
   } catch (error) {
     console.error(error);
