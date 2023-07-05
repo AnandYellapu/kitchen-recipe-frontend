1e13a9f80ddeeaf265b95d30b645e9fca8b16139
AnandYellapu <anandsaiii1200@gmail.com>
2023-07-05 08:20:01 +0530

    update

--- src/components/CreateFoodForm.js
+++ src/components/CreateFoodForm.js
@@ -1,143 +1,3 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { toast, ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import axios from 'axios';
-
-// function CreateFoodForm() {
-//   const [foodData, setFoodData] = useState({
-//     name: '',
-//     type: '',
-//     description: '',
-//     imageUrl: '',
-//     foodProcess: [],
-//     ingredients: [],
-//   });
-//   const navigate = useNavigate();
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     if (name === 'foodProcess') {
-//       const foodProcessSteps = value.split('\n');
-//       setFoodData((prevData) => ({
-//         ...prevData,
-//         [name]: foodProcessSteps,
-//       }));
-//     } else if (name === 'ingredients') {
-//       try {
-//         const ingredientList = JSON.parse(value);
-//         setFoodData((prevData) => ({
-//           ...prevData,
-//           ingredients: ingredientList,
-//         }));
-//       } catch (error) {
-//         console.error('Invalid JSON input:', error);
-//       }
-//     } else {
-//       setFoodData((prevData) => ({
-//         ...prevData,
-//         [name]: value,
-//       }));
-//     }
-//   };
-
-//   const createFood = async (foodData) => {
-//     try {
-//       await axios.post('https://kitchen-recipe.onrender.com/api/food', foodData);
-//     } catch (error) {
-//       console.error('Error creating food:', error);
-//       throw new Error('Failed to create food');
-//     }
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       await createFood(foodData);
-//       toast.success('Food created successfully!');
-//       navigate('/foods');
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <div className="create-food-container">
-//       <div className="create-food-form">
-//         <h2 className="form-heading">Create Food</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div className="form-field">
-//             <label htmlFor="name">Name:</label>
-//             <input
-//               type="text"
-//               id="name"
-//               name="name"
-//               value={foodData.name}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="form-field">
-//             <label htmlFor="type">Type:</label>
-//             <input
-//               type="text"
-//               id="type"
-//               name="type"
-//               value={foodData.type}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="form-field">
-//             <label htmlFor="description">Description:</label>
-//             <textarea
-//               id="description"
-//               name="description"
-//               value={foodData.description}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="form-field">
-//             <label htmlFor="imageUrl">Image URL:</label>
-//             <input
-//               type="text"
-//               id="imageUrl"
-//               name="imageUrl"
-//               value={foodData.imageUrl}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="form-field">
-//             <label htmlFor="foodProcess">Process:</label>
-//             <textarea
-//               id="foodProcess"
-//               name="foodProcess"
-//               value={foodData.foodProcess.join('\n')}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="form-field">
-//             <label htmlFor="ingredients">Ingredients:</label>
-//             <textarea
-//               id="ingredients"
-//               name="ingredients"
-//               value={JSON.stringify(foodData.ingredients, null, 2)}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <button type="submit" className="submit-button">Add on</button>
-//         </form>
-//       </div>
-//       <ToastContainer />
-//     </div>
-//   );
-// }
-
-// export default CreateFoodForm;
-
-
-
-
-
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
@@ -153,7 +13,6 @@ function CreateFoodForm() {
     foodProcess: [],
     ingredients: [],
   });
-  const [isFormComplete, setIsFormComplete] = useState(false);
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
@@ -180,17 +39,6 @@ function CreateFoodForm() {
         [name]: value,
       }));
     }
-
-    // Check if all fields are filled
-    const formFields = ['name', 'type', 'description', 'imageUrl', 'foodProcess', 'ingredients'];
-    const isComplete = formFields.every((field) => {
-      const fieldValue = foodData[field];
-      if (typeof fieldValue === 'string') {
-        return fieldValue.trim() !== '';
-      }
-      return !!fieldValue; // Check if field value exists
-    });
-    setIsFormComplete(isComplete);
   };
 
   const createFood = async (foodData) => {
@@ -203,28 +51,30 @@ function CreateFoodForm() {
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Check if any field is empty
-    const isEmpty = Object.values(foodData).some((value) => {
-      if (typeof value === 'string') {
-        return value.trim() === '';
-      }
-      return !value; // Check if field value exists
-    });
-    if (isEmpty) {
-      alert('Please fill in all the fields.');
-      return;
-    }
+  // Check if any required field is empty
+  if (
+    foodData.name.trim() === '' ||
+    foodData.type.trim() === '' ||
+    foodData.description.trim() === '' ||
+    foodData.imageUrl.trim() === '' ||
+    foodData.foodProcess.length === 0 ||
+    foodData.ingredients.length === 0
+  ) {
+    toast.error('Please fill in all the required fields');
+    return;
+  }
 
-    try {
-      await createFood(foodData);
-      toast.success('Food created successfully!');
-      navigate('/foods');
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  try {
+    await createFood(foodData);
+    toast.success('Food created successfully!');
+    navigate('/foods');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 
   return (
     <div className="create-food-container">
@@ -288,9 +138,7 @@ function CreateFoodForm() {
               onChange={handleInputChange}
             />
           </div>
-          <button type="submit" className="submit-button" disabled={!isFormComplete}>
-            Add on
-          </button>
+          <button type="submit" className="submit-button">Add on</button>
         </form>
       </div>
       <ToastContainer />
